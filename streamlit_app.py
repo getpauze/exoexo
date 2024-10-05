ce1bd962cd81427ea9409ca21c0a5db928921791
Meghana Seetharam <meghanadravidas@spotify.com>
2024-10-05 11:39:21 -0700

    Initial intro page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("Exoplanet Exploration")
+st.write("Your planet has been invaded by aliens! You need to find the exoplanet where they came from.")
+
+col1, col2, col3 = st.columns(3)
+
+with col2:
+    if st.button("Get Started"):
+        st.markdown(
+            """
+            <div style="display: flex; justify-content: center; align-items: center;">
+                <p>Button clicked!</p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
